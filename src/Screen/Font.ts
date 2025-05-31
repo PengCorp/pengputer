8aad4d7167797951f6b0af152307538d4bbb11f5
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-06-01 00:46:23 +0300

    Make font transparent

--- src/Screen/Font.ts
+++ src/Screen/Font.ts
@@ -40,21 +40,6 @@ export class Font {
 
     ctx.drawImage(bitmap, 0, 0);
 
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
-
-    for (let i = 0; i < data.length; i += 4) {
-      const r = data[i];
-      const g = data[i + 1];
-      const b = data[i + 2];
-
-      if (r === 0 && g === 0 && b === 0) {
-        data[i + 3] = 0;
-      }
-    }
-
-    ctx.putImageData(imageData, 0, 0);
-
     // load valueMap
 
     atlas.characterLocations = {};
