eb91e3399e5876eee4fe8059e850e94883703e2a
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-06-01 21:19:49 +0300

    Add makedir

--- src/Pengputer/main.ts
+++ src/Pengputer/main.ts
@@ -11,319 +11,6 @@ declare global {
   }
 }
 
-const programs = {
-  pong: function () {
-    window.open("/pongerslair", "_blank");
-    pc.screen.printString("PONG for PengOS (c) 1988 Ponger");
-  },
-
-  date: function () {
-    print(new Date().toDateString());
-  },
-
-  funk: async function () {
-    print("Playing CANYON.MID...");
-    canyonSong.fastSeek(0);
-    canyonSong.play();
-    await pressAnyKey();
-    canyonSong.pause();
-  },
-};
-
-const Program = Symbol();
-
-class Document {}
-
-class Image extends Document {
-  constructor(name, url) {
-    super();
-    this.name = name;
-    this.url = url;
-  }
-
-  open() {
-    window.open(this.url, "_blank");
-    print(`Opening ${this.name}...`);
-    print("Check your web browser's pop-up settings if nothing appears...");
-    print("");
-    print("...hey!");
-    print("What's a web browser?");
-  }
-}
-
-class AudioFile extends Document {
-  constructor(name, song) {
-    super();
-    this.name = name;
-    this.song = song;
-  }
-
-  async open() {
-    print(`Playing ${this.name}...`);
-    print("Press any key to exit.");
-    this.song.fastSeek(0);
-    this.song.play();
-    await pressAnyKey();
-    this.song.pause();
-  }
-}
-
-class TextFile extends Document {
-  constructor(text) {
-    super();
-    this.text = text;
-  }
-
-  open() {
-    print(this.text);
-  }
-}
-
-const licenseText =
-  "(C) COPYRIGHT 1985 PENGER CORPORATION (PENGCORP)\n\n" +
-  "BY VIEWING THIS FILE YOU ARE COMMITING A FELONY UNDER\n" +
-  "TITLE 2,239,132 SECTION XII OF THE PENGER CRIMINAL JUSTICE\nCODE";
-
-const fileSystem = {
-  pengos: {
-    "LICENSE.TXT": new TextFile(licenseText),
-  },
-  software: {
-    games: {
-      pong: {
-        "pong.exe": Program,
-      },
-    },
-  },
-  documents: {
-    pengers: {
-      "nerdger.png": new Image("NERDGER.PNG", "images/pengers/nerdger.png"),
-      "macger.png": new Image("MACGER.PNG", "images/pengers/macger.png"),
-    },
-    music: {
-      "canyon.mid": new AudioFile("CANYON.MID", canyonSong),
-      "passport.mid": new AudioFile("PASSPORT.MID", passportSong),
-      "king.mid": new AudioFile("KING.MID", kingSong),
-    },
-  },
-  "date.exe": Program,
-};
-
-const commands = [
-  function help() {
-    const helpText = {
-      help: "List available commands",
-      look: "Display contents of current directory",
-      go: "  Navigate directories",
-      up: "  Navigate to parent directory",
-      run: " Execute program",
-      open: "Display file",
-      take: "Add a program to the command list",
-      drop: "Remove a program from the command list",
-    };
-
-    for (let command of commands) {
-      if (helpText[command.name]) {
-        print(command.name + "   " + helpText[command.name]);
-      } else {
-        print(command.name + ": HELP ENTRY NOT FOUND");
-      }
-    }
-
-    if (extraCommands.length > 0) {
-      print("");
-      print("Available programs:");
-      for (let command of extraCommands) {
-        print(command);
-      }
-    }
-  },
-
-  function look() {
-    let dir = path[path.length - 1];
-
-    print("Currently in: " + pathNames.join("/"));
-    print("");
-
-    for (let name of Object.keys(dir)) {
-      if (isDirectory(dir[name])) {
-        print(name + "/");
-      } else {
-        print(name);
-      }
-    }
-  },
-
-  function go(name) {
-    let dir = path[path.length - 1];
-    let newDir = dir[name];
-
-    if (!newDir) {
-      print("Does not exist");
-    } else if (!isDirectory(newDir)) {
-      print("Not a directory");
-    } else {
-      path.push(newDir);
-      pathNames.push(name);
-    }
-
-    print("Now in " + pathNames.join("/"));
-  },
-
-  function up() {
-    if (path.length > 1) {
-      path = path.slice(0, path.length - 1);
-      pathNames = pathNames.slice(0, pathNames.length - 1);
-    }
-
-    print("Went up to " + pathNames.join("/"));
-  },
-
-  async function run(name) {
-    let dir = path[path.length - 1];
-    let program = dir[name];
-
-    if (isProgram(program)) {
-      return await programs[name.split(".")[0]]();
-    } else if (program === undefined) {
-      print("Does not exist");
-    } else {
-      print("Not executable");
-    }
-  },
-
-  async function open(name) {
-    let dir = path[path.length - 1];
-    let file = dir[name];
-
-    if (isFile(file)) {
-      await file.open();
-    } else if (file === undefined) {
-      print("Does not exist");
-    } else {
-      print("Not readable");
-    }
-  },
-
-  function take(name) {
-    let dir = path[path.length - 1];
-    let program = dir[name];
-
-    if (isProgram(program)) {
-      extraCommands.push(name.split(".")[0]);
-      localStorage.setItem("pengos/programs", JSON.stringify(extraCommands));
-      print("Added " + name.toUpperCase() + " to command list.");
-    } else if (program === undefined) {
-      print("Does not exist");
-    } else {
-      print("Not executable");
-    }
-  },
-
-  function drop(name) {
-    extraCommands = extraCommands.filter((x) => x != name);
-    localStorage.setItem("pengos/programs", JSON.stringify(extraCommands));
-  },
-];
-
-function pressAnyKey() {
-  return new Promise((resolve, reject) => {
-    let listener = function (event) {
-      document.removeEventListener("keydown", listener);
-      event.preventDefault();
-      resolve();
-    };
-    document.addEventListener("keydown", listener);
-  });
-}
-
-function isDirectory(thing) {
-  return !isProgram(thing) && !isFile(thing);
-}
-
-function isProgram(thing) {
-  return thing === Program;
-}
-
-function isFile(thing) {
-  return thing instanceof Document;
-}
-
-let path = [fileSystem];
-let pathNames = ["A:"];
-
-let extraCommands = [];
-{
-  let storedCommands = localStorage.getItem("pengos/programs");
-  if (storedCommands !== null) {
-    extraCommands = JSON.parse(storedCommands);
-  }
-}
-
-let print;
-
-function tab(input) {
-  let tokens = input.split(" ");
-  if (!tokens) return "";
-  let token = tokens[tokens.length - 1];
-
-  let dir = path[path.length - 1];
-  for (let name of Object.keys(dir)) {
-    let prefix = name.slice(0, token.length);
-    if (prefix === token) {
-      return name.slice(token.length);
-    }
-  }
-
-  return "";
-}
-
-async function submit(input, printFunction) {
-  let tokens = input.split(" ");
-
-  if (!tokens) return "";
-
-  let command = tokens[0];
-  let args = tokens.slice(1);
-
-  print = printFunction;
-
-  for (let f of commands) {
-    if (f.name == command || command == f.name[0]) {
-      await f(...args);
-      return;
-    }
-  }
-
-  if (extraCommands.includes(command)) {
-    let f = programs[command];
-    await f(...args);
-    return;
-  }
-
-  print("Unknown command: " + command);
-  print('Try "help" or "h" to see available commands');
-}
-
-// ====================== REAL
-
-enum FileSystemObjectType {
-  Directory = "dir",
-  TextFile = "txt",
-}
-
-type FileSystemEntry =
-  | {
-      type: FileSystemObjectType.Directory;
-      name: string;
-      data: Directory;
-    }
-  | {
-      type: FileSystemObjectType.TextFile;
-      name: string;
-      data: TextFile;
-    };
-
 class Directory {
   private items: FileSystemEntry[] = [];
 
@@ -345,6 +32,43 @@ class Directory {
     return newDir;
   }
 }
+
+class TextFile {
+  private data: string;
+
+  constructor() {
+    this.data = "";
+  }
+
+  getText() {
+    return this.data;
+  }
+
+  append(text: string) {
+    this.data = `${this.data}${text}`;
+  }
+
+  replace(text: string) {
+    this.data = text;
+  }
+}
+
+enum FileSystemObjectType {
+  Directory = "dir",
+  TextFile = "txt",
+}
+
+type FileSystemEntry =
+  | {
+      type: FileSystemObjectType.Directory;
+      name: string;
+      data: Directory;
+    }
+  | {
+      type: FileSystemObjectType.TextFile;
+      name: string;
+      data: TextFile;
+    };
 
 class FileSystem {
   private rootDir: Directory = new Directory();
@@ -413,6 +137,9 @@ class PengOS {
   }
 
   startup() {
+    window.startupNoise.volume = 0.7;
+    window.startupNoise.play();
+
     const { screen } = this.pc;
     screen.clear();
     screen.printString("PengOS 2.1\n(c) Copyright 1985 PengCorp\n");
@@ -494,6 +221,23 @@ class PengOS {
     }
   }
 
+  private commandMakedir(args: string[]) {
+    const { currentPath, fileSystem, screen } = this.pc;
+    const [newDirName] = args;
+    if (!newDirName) {
+      screen.printString("Must provide a name\n");
+    }
+
+    const currentDirEntry = fileSystem.getAtPath(currentPath);
+    if (currentDirEntry?.type === FileSystemObjectType.Directory) {
+      const currentDir = currentDirEntry.data;
+      currentDir.mkdir(newDirName);
+      screen.printString("Directory created\n");
+    } else {
+      screen.printString("Current path is not a directory\n");
+    }
+  }
+
   private commandHelp() {
     const { screen } = this.pc;
     screen.printString("help      List available commands\n");
@@ -508,12 +252,13 @@ class PengOS {
     const { screen, keyboard } = this.pc;
 
     const commands: Record<string, (args: string[]) => void> = {
-      prompt: this.commandPrompt.bind(this),
+      help: this.commandHelp.bind(this),
+      h: this.commandHelp.bind(this),
       look: this.commandLook.bind(this),
       go: this.commandGo.bind(this),
       up: this.commandUp.bind(this),
-      help: this.commandHelp.bind(this),
-      h: this.commandHelp.bind(this),
+      makedir: this.commandMakedir.bind(this),
+      prompt: this.commandPrompt.bind(this),
     };
 
     while (true) {
@@ -536,9 +281,6 @@ class PengOS {
 }
 
 (async () => {
-  window.startupNoise.volume = 0.7;
-  window.startupNoise.play();
-
   await loadFont9x16();
 
   const screen = new Screen();
