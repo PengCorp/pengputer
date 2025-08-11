9d735d3d78cf19afdf2f927985af5266c6adabf9
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-08-11 16:02:26 +0300

    Adjust render resolution slightly to be integer multiple of 320x240

--- src/Screen/constants.ts
+++ src/Screen/constants.ts
@@ -4,5 +4,5 @@ export const GRAPHICS_WIDTH = 320;
 export const GRAPHICS_HEIGHT = 240;
 export const GRAPHICS_RENDER_SCALE = 2;
 
-export const CANVAS_WIDTH = 1024;
-export const CANVAS_HEIGHT = 768;
+export const CANVAS_WIDTH = 960;
+export const CANVAS_HEIGHT = 720;
