82a48ef37aff5d42cab8be909e3a0da90decd629
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-08-12 02:33:20 +0300

    Update rendering resolution to 1024x768

--- src/Screen/constants.ts
+++ src/Screen/constants.ts
@@ -1,5 +1,5 @@
 export const GRAPHICS_WIDTH = 320;
 export const GRAPHICS_HEIGHT = 240;
 
-export const CANVAS_WIDTH = 960;
-export const CANVAS_HEIGHT = 720;
+export const CANVAS_WIDTH = 1024;
+export const CANVAS_HEIGHT = 768;
