7481d1612d3b6ee733fa658cd64254fa2672a451
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-08-12 16:34:02 +0300

    Random rectangles

--- src/Screen/Screen.gl.ts
+++ src/Screen/Screen.gl.ts
@@ -4,14 +4,15 @@ const vs = `#version 300 es
  
 // an attribute is an input (in) to a vertex shader.
 // It will receive data from a buffer
-in vec4 a_position;
+in vec2 a_position;
+
+uniform vec2 u_resolution;
  
 // all shaders have a main function
 void main() {
- 
   // gl_Position is a special variable a vertex shader
   // is responsible for setting
-  gl_Position = a_position;
+  gl_Position = vec4((a_position / u_resolution * 2.0 - 1.0) * vec2(1, -1), 0, 1);
 }`;
 
 const fs = `#version 300 es
@@ -19,13 +20,15 @@ const fs = `#version 300 es
 // fragment shaders don't have a default precision so we need
 // to pick one. highp is a good default. It means "high precision"
 precision highp float;
+
+uniform vec4 u_color;
  
 // we need to declare an output for the fragment shader
 out vec4 outColor;
  
 void main() {
   // Just set the output to a constant reddish-purple
-  outColor = vec4(1, 0, 0.5, 1);
+  outColor = u_color;
 }`;
 
 function createShader(
@@ -65,8 +68,42 @@ function createProgram(
   return null;
 }
 
-let positionAttributeLocation: number;
+// Returns a random integer from 0 to range - 1.
+function randomInt(range: number) {
+  return Math.floor(Math.random() * range);
+}
+
+function setRectangle(
+  gl: WebGL2RenderingContext,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+
+  // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
+  // whatever buffer is bound to the `ARRAY_BUFFER` bind point
+  // but so far we only have one buffer. If we had more than one
+  // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
+
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
+    gl.STATIC_DRAW,
+  );
+}
+
 let program: WebGLProgram;
+
+let positionAttributeLocation: number;
+let resolutionUniformLocation: WebGLUniformLocation;
+let colorUniformLocation: WebGLUniformLocation;
+
+let vao: WebGLVertexArrayObject;
 
 export const initScreen = (gl: WebGL2RenderingContext) => {
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vs)!;
@@ -75,21 +112,20 @@ export const initScreen = (gl: WebGL2RenderingContext) => {
   program = createProgram(gl, vertexShader, fragmentShader)!;
 
   positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")!;
+  colorUniformLocation = gl.getUniformLocation(program, "u_color")!;
+
+  const vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+  vao = gl.createVertexArray();
+  gl.bindVertexArray(vao);
+
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(positionAttributeLocation);
 };
 
 export const drawScreen = (gl: WebGL2RenderingContext) => {
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  const positions = [-1, 1, 1, 1, -1, -1];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_COPY);
-
-  const vao = gl.createVertexArray();
-  gl.bindVertexArray(vao);
-  gl.enableVertexAttribArray(positionAttributeLocation);
-
-  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
   gl.clearColor(0, 0, 1, 1);
@@ -97,7 +133,25 @@ export const drawScreen = (gl: WebGL2RenderingContext) => {
 
   gl.useProgram(program);
 
-  gl.bindVertexArray(vao);
+  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  for (var ii = 0; ii < 50; ++ii) {
+    setRectangle(
+      gl,
+      randomInt(300),
+      randomInt(300),
+      randomInt(300),
+      randomInt(300),
+    );
+
+    gl.uniform4f(
+      colorUniformLocation,
+      Math.random(),
+      Math.random(),
+      Math.random(),
+      1,
+    );
+
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+  }
 };
