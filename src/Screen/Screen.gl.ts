6e5efa982f07275230b400c1ed8a665e0997d0dc
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-08-12 21:17:10 +0300

    Grid of colors

--- src/Screen/Screen.gl.ts
+++ src/Screen/Screen.gl.ts
@@ -2,70 +2,42 @@ import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./constants";
 import { createProgram, createShader } from "./Screen.glut";
 
 const vs = `#version 300 es
- 
-// an attribute is an input (in) to a vertex shader.
-// It will receive data from a buffer
-in vec2 a_position;
+in vec4 a_position;
 
-uniform vec2 u_resolution;
- 
-// all shaders have a main function
 void main() {
-  // gl_Position is a special variable a vertex shader
-  // is responsible for setting
-  gl_Position = vec4((a_position / u_resolution * 2.0 - 1.0) * vec2(1, -1), 0, 1);
-}`;
+  gl_Position = a_position;
+}
+`;
 
 const fs = `#version 300 es
- 
-// fragment shaders don't have a default precision so we need
-// to pick one. highp is a good default. It means "high precision"
 precision highp float;
 
-uniform vec4 u_color;
- 
-// we need to declare an output for the fragment shader
+in vec2 v_texcoord;
+
+//uniform sampler2D u_texture;
+uniform vec2 u_resolution;
+
 out vec4 outColor;
- 
+uniform vec2 u_chars_grid_size;
+
 void main() {
-  // Just set the output to a constant reddish-purple
-  outColor = u_color;
-}`;
+  vec2 screen_coord = gl_FragCoord.xy * vec2(1.0, -1.0) + vec2(0.0, u_resolution.y);
 
-// Returns a random integer from 0 to range - 1.
-function randomInt(range: number) {
-  return Math.floor(Math.random() * range);
+  vec2 char_size = u_resolution / u_chars_grid_size;
+
+  vec2 grid_pos = floor(screen_coord.xy / char_size);
+
+  vec2 cell_color = grid_pos.xy / u_chars_grid_size.xy;
+
+  outColor = vec4(cell_color.xy,mod(grid_pos.x, 2.0),1);
 }
-
-function setRectangle(
-  gl: WebGL2RenderingContext,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-) {
-  var x1 = x;
-  var x2 = x + width;
-  var y1 = y;
-  var y2 = y + height;
-
-  // NOTE: gl.bufferData(gl.ARRAY_BUFFER, ...) will affect
-  // whatever buffer is bound to the `ARRAY_BUFFER` bind point
-  // but so far we only have one buffer. If we had more than one
-  // buffer we'd want to bind that buffer to `ARRAY_BUFFER` first.
-
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
-    gl.STATIC_DRAW,
-  );
-}
+`;
 
 let program: WebGLProgram;
 
 let positionAttributeLocation: number;
 let resolutionUniformLocation: WebGLUniformLocation;
-let colorUniformLocation: WebGLUniformLocation;
+let charsGridSizeUniformLocation: WebGLUniformLocation;
 
 let vao: WebGLVertexArrayObject;
 
@@ -78,16 +50,34 @@ export const initScreen = (gl: WebGL2RenderingContext) => {
   positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
   resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution")!;
-  colorUniformLocation = gl.getUniformLocation(program, "u_color")!;
+  charsGridSizeUniformLocation = gl.getUniformLocation(
+    program,
+    "u_chars_grid_size",
+  )!;
 
-  const vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  // prettier-ignore
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([
+      -1, 1,
+      1, 1,
+      -1, -1,
+      
+      1, 1,
+      1, -1,
+      -1, -1
+    ]),
+    gl.STATIC_DRAW,
+  );
 
   vao = gl.createVertexArray();
   gl.bindVertexArray(vao);
 
-  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(positionAttributeLocation);
+
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 };
 
 export const drawScreen = (gl: WebGL2RenderingContext) => {
@@ -100,24 +90,7 @@ export const drawScreen = (gl: WebGL2RenderingContext) => {
   gl.bindVertexArray(vao);
 
   gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+  gl.uniform2f(charsGridSizeUniformLocation, 80, 25);
 
-  for (var ii = 0; ii < 50; ++ii) {
-    setRectangle(
-      gl,
-      randomInt(300),
-      randomInt(300),
-      randomInt(300),
-      randomInt(300),
-    );
-
-    gl.uniform4f(
-      colorUniformLocation,
-      Math.random(),
-      Math.random(),
-      Math.random(),
-      1,
-    );
-
-    gl.drawArrays(gl.TRIANGLES, 0, 6);
-  }
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 };
