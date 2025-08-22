621314943b8e88ed4d73a42e02732a4f9525020a
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-08-23 01:31:03 +0300

    Rendering gradients

--- src/Screen/Screen.gl.ts
+++ src/Screen/Screen.gl.ts
@@ -2,9 +2,13 @@ import { CANVAS_HEIGHT, CANVAS_WIDTH } from "./constants";
 import { createProgram, loadShader } from "./Screen.utils";
 
 const vShaderStr = `#version 300 es
+
 in vec4 a_position;
 
+out vec2 v_screenPosition;
+
 void main() {
+  v_screenPosition = (a_position.xy * 0.5 + 0.5) * vec2(1, -1) + vec2(0, 1);
   gl_Position = a_position;
 }
 `;
@@ -12,22 +16,23 @@ void main() {
 const fShaderStr = `#version 300 es
 precision highp float;
 
+in vec2 v_screenPosition;
+
 out vec4 o_fragColor;
 
 void main() {
-  o_fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  o_fragColor = vec4(v_screenPosition.x, v_screenPosition.y, 0.0, 1.0);
 }
 `;
 
 let program: WebGLProgram;
 
+let vao: WebGLVertexArrayObject;
+let vbo: WebGLBuffer;
+
 let positionAttributeLocation: number;
 let resolutionUniformLocation: WebGLUniformLocation;
 let charsGridSizeUniformLocation: WebGLUniformLocation;
-
-let vao: WebGLVertexArrayObject;
-
-let vbo: WebGLBuffer;
 
 // prettier-ignore
 const quad = new Float32Array([ -1,-1, 1,-1, -1,1, 1,1 ]);
@@ -39,19 +44,20 @@ export const initScreen = (gl: WebGL2RenderingContext) => {
   program = createProgram(gl, vertexShader, fragmentShader)!;
   gl.useProgram(program);
 
+  positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
   vao = gl.createVertexArray();
   gl.bindVertexArray(vao);
-
-  positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
   gl.viewport(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
   vbo = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
   gl.bufferData(gl.ARRAY_BUFFER, quad, gl.STATIC_DRAW);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
   gl.bindVertexArray(null);
 };
 
