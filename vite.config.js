fcce09170883be068f34cb527b2e8435572dde58
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-09-05 22:50:06 +0300

    Set up test env

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/computer",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-      },
-    },
-  },
   resolve: {
     alias: {
       "@Toolbox": path.resolve(__dirname, "src/Toolbox"),
       "@Color": path.resolve(__dirname, "src/Color"),
       "@FileSystem": path.resolve(__dirname, "src/FileSystem"),
       "@src": path.resolve(__dirname, "src"),
+      "@src_test": path.resolve(__dirname, "src_test"),
     },
   },
 });
