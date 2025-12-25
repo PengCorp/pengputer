aaa98251efde63d10323306545d17fc572af40f5
Strawberry <112416885+properly-strawberry@users.noreply.github.com>
2025-12-25 02:22:05 +0200

    Move things around

--- vite.env.d.ts
+++ vite.env.d.ts
@@ -2,14 +2,17 @@ declare module "*.glsl?raw" {
   const src: string;
   export default src;
 }
+
 declare module "*.vert?raw" {
   const src: string;
   export default src;
 }
+
 declare module "*.frag?raw" {
   const src: string;
   export default src;
 }
+
 declare module "*.txt?raw" {
   const src: string;
   export default src;
